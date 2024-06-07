a3c8a7cc9e4fcdfe9e1d5d6e1a068adfabfbe7c3
Katie Russo <krusso196@gmail.com>
2024-06-07 00:45:17 -0400

    practicing comit no exclamation point

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
-	return "Hello world!";
-	// trying to get GitHub actions to register
+	return "Hello world";
+	// practicing commit no exclamation point
 }
  
 module.exports = hello;
